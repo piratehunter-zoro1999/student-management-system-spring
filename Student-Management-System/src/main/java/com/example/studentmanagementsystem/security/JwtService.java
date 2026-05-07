817680c99a1e41cd19126293d1050a59acdb74b3
kushpinder <kushpindersingh15@gmail.com>
2026-05-07 22:14:57 +0530

    Handle invalid JWT parsing errors with custom authentication exception

--- Student-Management-System/src/main/java/com/example/studentmanagementsystem/security/JwtService.java
+++ Student-Management-System/src/main/java/com/example/studentmanagementsystem/security/JwtService.java
@@ -10,17 +10,20 @@ public class JwtService {
 
     private final JwtUtil jwtutil = new JwtUtil();
 
-    public void validateToken(String authHeader){
+    public void validateToken(String authHeader) {
+        try {
+            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+                throw new JwtAuthenticationException(" Missing or Invalid Authorization Header");
+            }
+            String token = authHeader.substring(7);
+            String username = jwtutil.extractUsername(token);
 
-        if(authHeader == null || !authHeader.startsWith("Bearer ")){
-             throw new JwtAuthenticationException(" Missing or Invalid Authorization Header");
+            if (!jwtutil.validateToken(token, username)) {
+                throw new JwtAuthenticationException("Invalid or Expired token");
+            }
+
+        } catch (Exception e) {
+             throw new JwtAuthenticationException("invalid or Expired token");
         }
-        String token = authHeader.substring(7);
-        String username = jwtutil.extractUsername(token);
-
-        if(!jwtutil.validateToken(token,username)){
-             throw new JwtAuthenticationException("Invalid or token Expired");
-        }
-
     }
 }
